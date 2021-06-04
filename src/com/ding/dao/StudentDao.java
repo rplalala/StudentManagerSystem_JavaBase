72213fe79b34b7e77bae7662cf2bfaa27bd20a30
Ding <705220170@qq.com>
2021-06-04 16:40:29 +0800

    学生数据库操作接口

---
+++ src/com/ding/dao/StudentDao.java
@@ -0,0 +1,18 @@
+package com.ding.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ding.entity.StudentEntity;
+
+//学生接口，定义功能
+public interface StudentDao {
+	List<Map<String,String>> queryStudent(StudentEntity stu,int pageNum);
+	int addStudent(StudentEntity stu);
+	int deleteStudent(int id);
+	int updateStudent(StudentEntity stu);
+	void solveDeleteProblem();
+	int countData(StudentEntity stu);
+	String[] getFloors();
+	String[] getClasses();
+}
