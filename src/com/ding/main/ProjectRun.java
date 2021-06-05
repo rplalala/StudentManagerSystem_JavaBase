cc889f2e70463aa7201811289fcc4d316c4427f2
Ding <705220170@qq.com>
2021-06-05 12:12:54 +0800

    主程序入口

--- src/com/ding/main/ProjectRun.java
+++ src/com/ding/main/ProjectRun.java
@@ -4,8 +4,8 @@ import com.ding.ui.LoginUI;
 
 public class ProjectRun {
 
-	public static void main(String[] args) {
-		new LoginUI();
-	}
+    public static void main(String[] args) {
+        new LoginUI();
+    }
 
 }
