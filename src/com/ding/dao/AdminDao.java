7a4fdd636bb7942cc6462f9908c5f8a14d9f1e29
Ding <705220170@qq.com>
2021-06-05 12:08:51 +0800

    管理员操作接口

--- src/com/ding/dao/AdminDao.java
+++ src/com/ding/dao/AdminDao.java
@@ -2,8 +2,20 @@ package com.ding.dao;
 
 import com.ding.entity.AdminEntity;
 
+import java.util.List;
+import java.util.Map;
+
 //管理员接口，定义功能
 public interface AdminDao {
-	boolean isLogin(AdminEntity admin);
+    boolean isLogin(AdminEntity admin);
 
+    List<Map<String, String>> queryAdmin();
+
+    int countAdmin();
+
+    boolean updateAdmin(AdminEntity admin, String userName);
+
+    boolean deleteAdmin(String userName);
+
+    boolean addAdmin(AdminEntity admin);
 }
